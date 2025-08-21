af7e8d4efade43b4ce60e820aa55a3531e121151
sina.gilassi <sina.gilassi@gmail.com>
2025-08-21 00:40:36 -0400

    define two agent route

---
+++ pythermoai/api/data_agent.py
@@ -0,0 +1,17 @@
+# import libs
+import logging
+import asyncio
+import threading
+from fastapi import HTTPException, APIRouter, Request
+from fastapi.responses import JSONResponse
+# local
+
+# NOTE: logger
+logger = logging.getLogger(__name__)
+# set logging level
+logger.setLevel(logging.INFO)
+
+# SECTION: api router
+config_router = APIRouter(prefix="/data-agent")
+
+# SECTION: routes
